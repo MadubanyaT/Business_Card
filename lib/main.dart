f66f6b2822f4eda94c063294b70cf7a4863be72c
MrRob <mrrob9403@gmail.com>
2023-08-30 15:22:02 +0100

    Final Application

--- lib/main.dart
+++ lib/main.dart
@@ -9,38 +9,87 @@ class BusinessCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey.shade900,
         body: SafeArea(
           child: Column(
-            //Allows the Avatar to move freely in the row
-            mainAxisSize: MainAxisSize.min,
-            //put the avatar at center of the row
-            crossAxisAlignment: CrossAxisAlignment.center,
+            //Allows the Avatar to move freely in the column
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               //push the avatar down
-              SizedBox(
-                height: 200,
-                width: double.infinity,
-              ),
 
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
                 backgroundImage: ExactAssetImage('images/TL.jpg'),
                 backgroundColor: Colors.transparent,
               ),
-
-              Text(
+              const Text(
                 'Device_Particle',
                 style: TextStyle(
                   fontSize: 40,
                   fontWeight: FontWeight.w900,
                   color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontFamily: 'Open Sans',
+                  fontFamily: 'Pacifico',
                 ),
               ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade200,
+                  fontFamily: 'Source Code Pro',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.teal.shade200,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                 child: ListTile(
+                   leading: Icon(Icons.phone,
+                       color: Colors.blueGrey.shade900,
+                   ),
+                   title: const Text('+12 783 883 4400',
+                       style: TextStyle(
+                           color: Colors.black45,
+                           fontWeight: FontWeight.bold,
+                           fontSize: 20,
+                           fontFamily: 'Open Sans'
+                       ),
+                 ),
+                ),
+               ),
+              ),//phone
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email,
+                      color: Colors.blueGrey.shade900,
+                    ),
+                    title: const Text('QLab@gmail.com',
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontFamily: 'Open Sans'
+                    ),
+                  ),
+                ),
+              ),
+              ), //email
             ],
           ),
         ),
